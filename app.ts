402aa018b0c04a55b94ae27ae4b74700e62ab1f5
Muritala David <davmuri1414@gmail.com>
2023-04-09 12:48:35 +0100

    host url

--- app.ts
+++ app.ts
@@ -5,5 +5,7 @@ const hermes = new Hermes(ApplicationConfig)
 
 const server = hermes.server.listen({ host: ApplicationConfig.host, port: ApplicationConfig.port }, () => {
   const { port } = server.address() as any;
-  console.log(`Hermes broker started on https://${process.env.HOST}:${port}`);
+  const dyno = process.env.DYNO || 'web.1';
+  const url = `http://${dyno}.herokuapp.com:${port}`;
+  console.log(`Hermes broker started on ${url}`);
 });
