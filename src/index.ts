5130cdfb7bad7815535d5da597ac6e27b47a8e71
Muritala David <davmuri1414@gmail.com>
2023-04-10 09:15:39 +0100

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Hermes } from './server';
 export { HermesPublisher } from './publisher';
 export { HermesSubscriber } from './subscriber';
+export { HermesConnectionType, ConnectionOptions, Subscriber, MessageType } from './types/hermes.types'
