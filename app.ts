5978f66a8f25d6c603caea33233b8265bddcd2b4
Muritala David <davmuri1414@gmail.com>
2023-04-10 07:33:49 +0100

    update

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import { ApplicationConfig } from './src/config';
 
 const hermes = new Hermes(ApplicationConfig)
 
-const server = hermes.server.listen({ host: ApplicationConfig.host, port: ApplicationConfig.port }, () => {
-  const { host, port } = server.address() as any;
+const hermesServer = hermes.server.listen({ host: ApplicationConfig.host, port: ApplicationConfig.port }, () => {
+  const { host, port } = hermesServer.address() as any;
   console.log(`Hermes message broker started on ${host}:${port}`);
 });
